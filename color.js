aaea493e936baeb17e57254c541b91794272f20c
Haris Khan <54500301+theHarisKhan@users.noreply.github.com>
2020-06-30 11:35:00 +0500

    Add files via upload
    Added a button which will also generate random linear-gradient background-color
    By following DRY pattern make a seperate function of loop

--- color.js
+++ color.js
@@ -1,17 +1,37 @@
 const colors = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
 
-const btn = document.querySelector('.js-btn');
+const simpBtn = document.querySelector('.js-btn');
+const gradBtn = document.querySelector('.grad-btn');
 const color = document.querySelector('.color');
 
-btn.addEventListener('click', () => {
+//Simple Background-color generate
+simpBtn.addEventListener('click', ()=> {
     //to collect 6digit hex color we use
-    let Hexcolor = '#';
-    for(let i=0; i<6; i++){
-        Hexcolor += colors[RandNumb()];
-    }
-    color.textContent = Hexcolor;
-    document.body.style.backgroundColor = Hexcolor;
+    let HexColor = '#';
+    HexColor = loopThrough(HexColor);
+    color.textContent = HexColor;
+    document.body.style.background = HexColor;
 });
+
+//Gradient Background-color generate
+gradBtn.addEventListener('click', ()=> {
+    let gradColor1 = '#';
+    let gradColor2 = '#';
+
+    gradColor1 = loopThrough(gradColor1);
+    gradColor2 = loopThrough(gradColor2);
+
+    document.body.style.background = `linear-gradient(to right, ${gradColor1}, ${gradColor2}`;
+    color.textContent = `linear-grad(to right, ${gradColor1} , ${gradColor2})`;
+});
+
+//DRY(Don't Repeat Yourself) pattern
+function loopThrough(value){
+    for (let i=0; i<6; i++){
+        value += colors[RandNumb()];
+    }
+    return value;
+}
 
 function RandNumb(){
     return Math.floor(Math.random() * colors.length);
